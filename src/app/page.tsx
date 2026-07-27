99ec99d33582b851ed1dd67423c78ab874d878d2
kadalcoklat <kadalcoklat@pop-os.taila37d8d.ts.net>
2026-07-27 23:58:07 +0700

    feat: redirect root path / to /login unconditionally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const session = await auth();
-
-  if (session) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+export default function Home() {
+  redirect("/login");
 }
+
